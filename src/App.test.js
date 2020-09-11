d6085906c101e02f48a4283c5cb5980e0ec08a7a
Phil <philip-lisak@lambdastudents.com>
2020-09-10 19:47:01 -0700

    1st test failed. Probably calling the input field incorrectly

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,23 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, getByName } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+const inputTest = () => {
+  const inputs = render(<ContactForm />)
+  const fields = inputs.getByName('firstName');
+  return {
+    fields,
+    ...inputs,
+  }
+}
+
+test('user can type in input fields', () => {
+  const { fields } = inputTest();
+  fireEvent.change(fields, {target: {value: 'hello'}})
+  expect(fields.value).toBe('hello')
+})
